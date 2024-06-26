8f79c2748583109ba53d4a963480af38d1ab74ed
Senya0101 <e.palicheva999@gmail.com>
2024-06-26 22:10:16 +0900

    Step 2: Add arguments and option to help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
 
 program.parse();

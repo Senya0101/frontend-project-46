765db6c4b9539ca6cc0d909be9ee0ddf7ebeb0e4
Senya0101 <e.palicheva999@gmail.com>
2024-06-27 17:25:56 +0900

    Step 6: Add functionality to gendiff between two flat YAML files

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -9,8 +9,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixtureFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const expOutput = readFixtureFile('expRes1.txt');
 
 test('Check diff for flat JSON file', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expOutput);
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFixtureFile('expRes1.txt'));
+  });
+
+  test('Check diff for flat YAML file', () => {
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFixtureFile('expRes2.txt'));
   });
--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,19 @@
 import fs from 'fs';
-import { resolve } from 'path';
+import { resolve, extname } from 'path';
 import { cwd } from 'process';
+import yaml from 'js-yaml'
 
 const getAbsolutePath = (filepath) => resolve(cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf8');
+const getExtention = (filepath) => extname(filepath).substring(1);
 
 export default(filepath) => {
-    return JSON.parse(readFile(getAbsolutePath(filepath)))
+    let data = readFile(getAbsolutePath(filepath));
+    switch (getExtention(filepath)) {
+        case 'json':
+          return JSON.parse(data);
+        case 'yml':
+        case 'yaml':
+          return yaml.load(data);
+      }
 }

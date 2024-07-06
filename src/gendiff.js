dd6e7307fb2f8897fecd0cb1a9873f01a6b92e8f
Senya0101 <e.palicheva999@gmail.com>
2024-07-06 19:31:32 +0900

    Reformat gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
- const genDiff = (data1, data2) => {
+const genDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   return keys.map((key) => {

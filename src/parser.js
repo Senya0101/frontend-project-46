8a28b0e9173b1f6a780231e10e278cb29fe7365a
Senya0101 <e.palicheva999@gmail.com>
2024-07-06 17:43:41 +0900

    Correct gendiff to read absolute path

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,14 @@
 import fs from 'fs';
-import { resolve, extname } from 'path';
+import { resolve, extname, isAbsolute } from 'path';
 import { cwd } from 'process';
 import yaml from 'js-yaml'
 
-const getAbsolutePath = (filepath) => resolve(cwd(), filepath);
+const getAbsolutePath = (filepath) => {
+  if (isAbsolute(filepath)) {
+    return filepath;
+  }
+  return resolve(cwd(), filepath);
+};
 const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf8');
 const getExtention = (filepath) => extname(filepath).substring(1);
 

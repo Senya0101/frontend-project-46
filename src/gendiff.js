3084d4a4ec1eb7c0c626bf4791a52b68b024d88f
Senya0101 <e.palicheva999@gmail.com>
2024-07-06 19:21:28 +0900

    Fix bug

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-export default(data1, data2) => {
+export default genDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   return keys.map((key) => {
